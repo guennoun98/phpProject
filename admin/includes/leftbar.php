d91faaa027e71fb3af1943defe4f1cff162b8d9f
guennoun mohammed <guennoun98@hotmail.fr>
2020-01-09 19:53:45 +0100

    admin part1
    guennoun mohammed

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,36 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Create Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
+					<ul>
+						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+
+				
+				
+				
+
+				<li><a href="#"><i class="fa fa-sitemap"></i> Account</a>
+					<ul>
+						<li><a href="logout.php">logout</a></li>
+					</ul>
+				</li>
+			
+
+			
+
+			</ul>
+		</nav>
